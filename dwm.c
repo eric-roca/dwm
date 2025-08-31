d089312e7e83c9e4b1b3f97ddea3fabf8d93dadb
Eric Roca <eric.roca@gmail.com>
2025-08-31 18:24:12 +0200

    Adds exitmenu

--- dwm.c
+++ dwm.c
@@ -63,6 +63,23 @@
 #define TAGSLENGTH              (LENGTH(tags))
 #define TEXTW(X)                (drw_fontset_getwidth(drw, (X)) + lrpad)
 
+#define S_LOCK "Lock"
+#define S_RESTART_DWM "restart Dwm"
+#define S_OFFSCREEN "Off-screen"
+#define S_EXIT "Exit"
+#define S_REBOOT "Reboot"
+#define S_SHUTDOWN "Shutdown"
+/* Uses nerdfonts */
+#define S_LOCK_ICON "\uf023"
+#define S_RESTART_DWM_ICON "\uf01e"
+#define S_OFFSCREEN_ICON "\uf108"
+#define S_EXIT_ICON "\uf2f5"
+#define S_REBOOT_ICON "\uf021"
+#define S_SHUTDOWN_ICON "\uf011"
+
+#define S_FORMAT(ACTION) S_##ACTION##_ICON " " S_##ACTION
+#define S_FORMAT_CLEAR "sed 's/^..//'"
+
 /* enums */
 enum { CurNormal, CurResize, CurMove, CurLast }; /* cursor */
 enum { SchemeNorm, SchemeSel }; /* color schemes */
@@ -251,6 +268,7 @@ static int xerror(Display *dpy, XErrorEvent *ee);
 static int xerrordummy(Display *dpy, XErrorEvent *ee);
 static int xerrorstart(Display *dpy, XErrorEvent *ee);
 static void zoom(const Arg *arg);
+void exitdwm ();
 
 static pid_t getparentprocess(pid_t p);
 static int isdescprocess(pid_t p, pid_t c);
@@ -1455,7 +1473,7 @@ propertynotify(XEvent *e)
 void
 quit(const Arg *arg)
 {
-	running = 0;
+       running = 0;
 }
 
 Monitor *
@@ -2591,4 +2609,40 @@ main(int argc, char *argv[])
 	cleanup();
 	XCloseDisplay(dpy);
 	return EXIT_SUCCESS;
+}
+
+void exitdwm ()
+{
+	FILE * exit_menu = popen (
+		"echo \""
+			S_FORMAT (LOCK) "\n"
+			S_FORMAT (RESTART_DWM) "\n"
+			S_FORMAT (OFFSCREEN) "\n"
+			S_FORMAT (EXIT) "\n"
+			S_FORMAT (REBOOT) "\n"
+			S_FORMAT (SHUTDOWN)
+			"\" | dmenu -p exit: | " S_FORMAT_CLEAR
+		,
+		"r"
+	);
+
+	char exit_action [16];
+
+	if (
+		exit_menu == NULL ||
+		fscanf (exit_menu, "%15[a-zA-Z -]", exit_action) == EOF
+	) {
+		fputs ("Error. Failure in exit_dwm.", stderr);
+		goto close_streams;
+	}
+
+	if (strcmp (exit_action, S_LOCK) == 0) system ("slock & sleep .5; xset dpms force off");
+	else if (strcmp (exit_action, S_RESTART_DWM) == 0) quit (& (const Arg) {1});
+	else if (strcmp (exit_action, S_OFFSCREEN) == 0) system ("sleep .5; xset dpms force off");
+	else if (strcmp (exit_action, S_EXIT) == 0) quit (& (const Arg) {0});
+	else if (strcmp (exit_action, S_REBOOT) == 0) system ("systemctl reboot");
+	else if (strcmp (exit_action, S_SHUTDOWN) == 0) system ("systemctl poweroff -i");
+
+close_streams:
+	pclose (exit_menu);
 }
